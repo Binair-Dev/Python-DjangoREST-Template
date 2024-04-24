9a28c9d40d0a8ef99e85cee748fdfe4236b089cc
Brian Van Bellinghen <b_nair.corp@yahoo.com>
2024-04-24 10:06:57 +0200

    basic to api view

--- videotheque/movie/views.py
+++ videotheque/movie/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt 
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from .models import *
 from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+#Create your views here.
+#API VIEW Template
 @csrf_exempt 
+@api_view(['GET', 'POST'])
 def director_list_view(request):
     if request.method == 'GET':
         directors = Director.objects.all()
         serializer = DirectorSerializer(directors, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DirectorSerializer(data=data)
+        serializer = DirectorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 @csrf_exempt
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def director_detail_view(request, id):
     try:
         director = Director.objects.get(id=id)
@@ -30,72 +34,164 @@ def director_detail_view(request, id):
     
     if request.method == 'GET':
         serializer = DirectorSerializer(director)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = DirectorSerializer(director, data=data)
+        serializer = DirectorSerializer(director, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'PATCH':
-        data = JSONParser().parse(request)
-        serializer = DirectorSerializer(director, data=data, partial=True)
+        serializer = DirectorSerializer(director, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'DELETE':
         director.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 @csrf_exempt 
+@api_view(['GET', 'POST'])
 def movie_list_view(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(data=data)
+        serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 @csrf_exempt
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def movie_details_view(request, id):
     try:
         movie = Movie.objects.get(id=id)
     except Movie.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(movie, data=data)
+        serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'PATCH':
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(movie, data=data, partial=True)
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'DELETE':
         movie.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+#BASIC Template
+# @csrf_exempt 
+# def director_list_view(request):
+#     if request.method == 'GET':
+#         directors = Director.objects.all()
+#         serializer = DirectorSerializer(directors, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = DirectorSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    
+# @csrf_exempt
+# def director_detail_view(request, id):
+#     try:
+#         director = Director.objects.get(id=id)
+#     except Director.DoesNotExist:
+#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         serializer = DirectorSerializer(director)
+#         return JsonResponse(serializer.data)
+
+#     if request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = DirectorSerializer(director, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+        
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'PATCH':
+#         data = JSONParser().parse(request)
+#         serializer = DirectorSerializer(director, data=data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+        
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'DELETE':
+#         director.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+    
+# @csrf_exempt 
+# def movie_list_view(request):
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = MovieSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    
+# @csrf_exempt
+# def movie_details_view(request, id):
+#     try:
+#         movie = Movie.objects.get(id=id)
+#     except Movie.DoesNotExist:
+#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         serializer = MovieSerializer(movie)
+#         return JsonResponse(serializer.data)
+
+#     if request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = MovieSerializer(movie, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'PATCH':
+#         data = JSONParser().parse(request)
+#         serializer = MovieSerializer(movie, data=data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+        
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'DELETE':
+#         movie.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
