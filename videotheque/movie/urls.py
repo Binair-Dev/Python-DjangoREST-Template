12102a5ce405882b482f3e0da54721ff479f43d7
Brian Van Bellinghen <b_nair.corp@yahoo.com>
2024-04-25 14:14:51 +0200

    customised automatisation

--- videotheque/movie/urls.py
+++ videotheque/movie/urls.py
@@ -3,10 +3,39 @@ from django.urls.conf import include
 from .views import DirectorViewset, MovieViewset
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'directors', DirectorViewset)
-router.register(r'movies', MovieViewset)
+# router = DefaultRouter()
+# router.register(r'directors', DirectorViewset)
+# router.register(r'movies', MovieViewset)
+
+director_list = DirectorViewset.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+director_detail = DirectorViewset.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+movie_list = MovieViewset.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+movie_detail = MovieViewset.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    path('', include(router.urls))
+    #path('', include(router.urls))
+    path('directors/', director_list, name='director-list'),
+    path('directors/<int:pk>/', director_detail, name='director-detail'),
+
+    path('movies/', movie_list, name='movie-list'),
+    path('movies/<int:pk>/', movie_detail, name='movie-detail'),
 ]
